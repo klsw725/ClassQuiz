c3a3319e50e7e52293119bc0767461199c054231
Seungwoo Lee <klswX725@gmail.com>
2026-05-20 18:03:17 +0900

    :bug: Allow three-character player names

--- frontend/src/lib/play/join.svelte
+++ frontend/src/lib/play/join.svelte
@@ -123,7 +123,7 @@ SPDX-License-Identifier: MPL-2.0
 
 	const setUsername = async (e: Event) => {
 		e.preventDefault();
-		if (username.length <= 3) {
+		if (username.length < 3) {
 			return;
 		}
 		let captcha_resp: string;
@@ -258,7 +258,7 @@ SPDX-License-Identifier: MPL-2.0
 			{/if}
 
 			<div class="mt-2">
-				<BrownButton disabled={username.length <= 3} onclick={setUsername}
+				<BrownButton disabled={username.length < 3} onclick={setUsername}
 					>{$t('words.submit')}</BrownButton
 				>
 			</div>
